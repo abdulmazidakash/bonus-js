5ed0a92ee10448016327d66472e3df1b982aa9b7
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-03 15:04:54 +0600

    learning declared variable for loop bonus recap js

---
+++ bonus.js
@@ -0,0 +1,21 @@
+/**let number = 59;
+number = 56;
+number = 75;
+
+const name = 'Asad';
+console.log(number);
+*/
+
+/**var marks = [54, 23, 54, 76, 78, 75, 34];
+
+for( var i = 0; i < marks.length; i++){
+	console.log('hello');
+}
+*/
+
+var marks = [54, 23, 54, 76, 78, 75, 34];
+
+for( var i = 0; i < marks.length; i++){
+	var element = marks[i];
+	console.log(element);
+}
